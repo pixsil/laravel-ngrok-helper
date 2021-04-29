2468dfb98eb264272e00477b1ef8f58f4af3b71b
pixsil <57232302+pixsil@users.noreply.github.com>
2021-04-29 10:29:54 +0200

    Update NgrokHelper.php

--- helpers/NgrokHelper.php
+++ helpers/NgrokHelper.php
@@ -1,25 +1,26 @@
 <?php
 
-// verion 3
+// version 4
 if (!function_exists('ngrok_replace')) {
     function ngrok_replace($url)
     {
         // guard live
-        if (env('USE_NGROK_REPLACE', false) === false) {
+        if (env('NGROK_REPLACE', false) === false) {
             return $url;
         }
         // guard no ngrok
         if (!isset($_SERVER['HTTP_X_ORIGINAL_HOST'])) {
             abort(403, 'Looks like you are not using Ngrok?');
         }
-    
+
         // replace url
         $url = str_replace($_SERVER['HTTP_HOST'], $_SERVER['HTTP_X_ORIGINAL_HOST'], $url);
 
-
         // replace https
-        $url = str_replace('http://', 'https://', $url);
-
+        if (env('NGROK_FORCE_SSL', true) === true) {
+            $url = str_replace('http://', 'https://', $url);
+        }
+        
         return $url;
     }
 }
